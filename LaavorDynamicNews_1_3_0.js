371e902cc4d75778aefaf52ae7181bfab3332c55
laavor <39971288+laavor@users.noreply.github.com>
2018-11-26 12:41:59 -0200

    Version 1.3.0
    Added new methods for manual navigation:
    LaavorMoveToPrevious ();
    LaavorMoveToNext ();

---
+++ LaavorDynamicNews_1_3_0.js
@@ -0,0 +1,47 @@
+﻿var _yry_Turner_Stark_swagahaha;var SG1__Ancients_;var SG1__Aschen_;var SG1__Asgard_;var SG1__Asurans_;var SG1__A_t_trr_;var SG1__Crystalline_species_;function LaavorDynamicNews(democracy,hilmarsdottir){_yry_Turner_Stark_swagahaha=false;var __lauytegdfdr=document.getElementById(democracy);if(__lauytegdfdr!==undefined&&__lauytegdfdr!==null){SG1__A_t_trr_=__lauytegdfdr;var countryTaylor__iliiii7777=mcxncvnxnnm_change_ldsdsakadskl_discovery_hsdhgfdsjhg();var Karen_____Pond_1234567;if(hilmarsdottir.pauseInHover!==undefined&&hilmarsdottir.pauseInHover!==null){Karen_____Pond_1234567=hilmarsdottir.pauseInHover;}
+else{Karen_____Pond_1234567=countryTaylor__iliiii7777.pauseInHover;}
+if(Karen_____Pond_1234567.toString()==="true")
+{__lauytegdfdr.onmouseover=Fringe;__lauytegdfdr.onmouseleave=Person_of_Interest;}
+var again_40_280_7_EasterEgg_ZEC;if(hilmarsdottir.elementItemHtml!==undefined&&hilmarsdottir.elementItemHtml!==null){again_40_280_7_EasterEgg_ZEC=hilmarsdottir.elementItemHtml;}
+else{again_40_280_7_EasterEgg_ZEC=countryTaylor__iliiii7777.elementItemHtml;}
+var Bar_asjdhkjashdkj_40thol_jahahahah_di=__lauytegdfdr.getElementsByTagName(again_40_280_7_EasterEgg_ZEC);if(Bar_asjdhkjashdkj_40thol_jahahahah_di!==undefined&&Bar_asjdhkjashdkj_40thol_jahahahah_di!==null)
+{var z40_index_boston_robot___ksakldaasdasdasd;if(hilmarsdottir.numberVisibleItems!==undefined&&hilmarsdottir.numberVisibleItems!==null){z40_index_boston_robot___ksakldaasdasdasd=hilmarsdottir.numberVisibleItems;}
+else{z40_index_boston_robot___ksakldaasdasdasd=countryTaylor__iliiii7777.numberVisibleItems;}
+SG1__Asgard_=z40_index_boston_robot___ksakldaasdasdasd;for(var _margot_free_7=0;_margot_free_7<Bar_asjdhkjashdkj_40thol_jahahahah_di.length;_margot_free_7++){if(_margot_free_7+1<=z40_index_boston_robot___ksakldaasdasdasd){Bar_asjdhkjashdkj_40thol_jahahahah_di[_margot_free_7].style.display="block";}
+else{Bar_asjdhkjashdkj_40thol_jahahahah_di[_margot_free_7].style.display="none";}}
+var _civil_vancamp;if(hilmarsdottir.speed!==undefined&&hilmarsdottir.speed!==null){_civil_vancamp=hilmarsdottir.speed;}
+else{_civil_vancamp=countryTaylor__iliiii7777.speed;}
+SG1__Asurans_=_civil_vancamp;var _42_42_42_Star_Galactica_zzzz_ceacaracatra_=[];for(var _Grays_78788888_tone_Caprica_=0;_Grays_78788888_tone_Caprica_<Bar_asjdhkjashdkj_40thol_jahahahah_di.length;_Grays_78788888_tone_Caprica_++){_42_42_42_Star_Galactica_zzzz_ceacaracatra_.push(Bar_asjdhkjashdkj_40thol_jahahahah_di[_Grays_78788888_tone_Caprica_].cloneNode(true));}
+SG1__Aschen_=_42_42_42_Star_Galactica_zzzz_ceacaracatra_;clearTimeout(SG1__Crystalline_species_);SG1__Ancients_=0;SG1__Crystalline_species_=setTimeout(function(){RosettaStone(1,_42_42_42_Star_Galactica_zzzz_ceacaracatra_,z40_index_boston_robot___ksakldaasdasdasd,_civil_vancamp,__lauytegdfdr);},_civil_vancamp);}
+else
+{throw new Error("The elementItemHtml 'children' was not found inside the parent.");}}
+else
+{throw new Error("Item with idFather informed was not found.");}}
+function Fringe()
+{_yry_Turner_Stark_swagahaha=true;}
+function Person_of_Interest()
+{_yry_Turner_Stark_swagahaha=false;}
+function LaavorMoveToPrevious(){SG1();}
+function LaavorMoveToNext(){SGAtlantis();}
+function SG1(){if(SG1__Ancients_!==undefined&&SG1__Ancients_!==null&&SG1__Aschen_!==undefined&&SG1__Aschen_!==null&&SG1__Asgard_!==undefined&&SG1__Asgard_!==null&&SG1__Asurans_!==undefined&&SG1__Asurans_!==null&&SG1__A_t_trr_!==undefined&&SG1__A_t_trr_!==null){isStoped=false;var SG1__Berzerker_Drones_;if(SG1__Ancients_-1>=0){SG1__Berzerker_Drones_=SG1__Ancients_-1;}
+else{SG1__Berzerker_Drones_=SG1__Aschen_.length-1;}
+clearTimeout(SG1__Crystalline_species_);RosettaStone(SG1__Berzerker_Drones_,SG1__Aschen_,SG1__Asgard_,SG1__Asurans_,SG1__A_t_trr_);}
+else{throw new Error('Incomplete or invalid data for function call - "LaavorMoveToPrevious".');}}
+function SGAtlantis(){if(SG1__Ancients_!==undefined&&SG1__Ancients_!==null&&SG1__Aschen_!==undefined&&SG1__Aschen_!==null&&SG1__Asgard_!==undefined&&SG1__Asgard_!==null&&SG1__Asurans_!==undefined&&SG1__Asurans_!==null&&SG1__A_t_trr_!==undefined&&SG1__A_t_trr_!==null){isStoped=false;var SG1__Dust_Bugs_;if(SG1__Ancients_+1>=SG1__Aschen_.length){SG1__Dust_Bugs_=0;}
+else{SG1__Dust_Bugs_=SG1__Ancients_+1;}
+clearTimeout(SG1__Crystalline_species_);RosettaStone(SG1__Dust_Bugs_,SG1__Aschen_,SG1__Asgard_,SG1__Asurans_,SG1__A_t_trr_);}
+else{throw new Error('Incomplete or invalid data for function call - "LaavorMoveToNext".');}}
+function RosettaStone(liberte_F,_7_40_280_lalala_Grea_t_Charter,numberVisibleItems,fraternite_F,_42_another_42__Goa_uld){var _15_jun_1215=0;var democrazia_1946=liberte_F;var demo_athens_kracia=[];var _0_0_17_b_1_i_8_a=0;if(_yry_Turner_Stark_swagahaha.toString()==="false")
+{_42_another_42__Goa_uld.innerHTML="";while(_0_0_17_b_1_i_8_a<_7_40_280_lalala_Grea_t_Charter.length){if(liberte_F>=_7_40_280_lalala_Grea_t_Charter.length){break;}
+if(demo_athens_kracia.indexOf(democrazia_1946)>=0){break;}
+if(demo_athens_kracia.length===numberVisibleItems){break;}
+demo_athens_kracia.push(democrazia_1946);if(_15_jun_1215<numberVisibleItems){_42_another_42__Goa_uld.appendChild(_7_40_280_lalala_Grea_t_Charter[democrazia_1946]);_7_40_280_lalala_Grea_t_Charter[democrazia_1946].style.display="block";}
+else{_7_40_280_lalala_Grea_t_Charter[democrazia_1946].style.display="none";}
+democrazia_1946++;if(democrazia_1946===_7_40_280_lalala_Grea_t_Charter.length){democrazia_1946=0;}
+_15_jun_1215++;_0_0_17_b_1_i_8_a++;}
+SG1__Ancients_=liberte_F;var _lasklasdlkjasdlk_jkhasdkjasdkjh_1783;if(liberte_F+1<_7_40_280_lalala_Grea_t_Charter.length){_lasklasdlkjasdlk_jkhasdkjasdkjh_1783=liberte_F+1;}
+else{_lasklasdlkjasdlk_jkhasdkjasdkjh_1783=0;}
+clearTimeout(SG1__Crystalline_species_);SG1__Crystalline_species_=setTimeout(function(){RosettaStone(_lasklasdlkjasdlk_jkhasdkjasdkjh_1783,_7_40_280_lalala_Grea_t_Charter,numberVisibleItems,fraternite_F,_42_another_42__Goa_uld);},fraternite_F);}
+else
+{clearTimeout(SG1__Crystalline_species_);SG1__Crystalline_species_=setTimeout(function(){RosettaStone(liberte_F,_7_40_280_lalala_Grea_t_Charter,numberVisibleItems,fraternite_F,_42_another_42__Goa_uld);},fraternite_F);}}
+function mcxncvnxnnm_change_ldsdsakadskl_discovery_hsdhgfdsjhg(){return{speed:3500,pauseInHover:false,numberVisibleItems:3,elementItemHtml:'span'};}
